24dd706e755ea3f45a20f41b9cecc4051a1c3bb3
Egor Pavlenko <e.pavlenko@aag-it.pro>
2025-05-17 02:11:17 +0300

    swagger

--- WebStudents/Program.cs
+++ WebStudents/Program.cs
@@ -38,7 +38,11 @@ builder.WebHost.ConfigureKestrel(options =>
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Student API v1");
+    c.RoutePrefix = "swagger";
+});
 
 if (!app.Environment.IsDevelopment())
 {
--- WebStudents/obj/Debug/net8.0/WebStudents.AssemblyInfo.cs
+++ WebStudents/obj/Debug/net8.0/WebStudents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStudents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d0dc5b1ac2d65dd95b70eae496740e1c0d2be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStudents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStudents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
